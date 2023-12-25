d5583bbb492999ecdacded02aee996d39ab4aea7
justinlime <justinlime1999@gmail.com>
2023-12-25 01:49:52 -0600

    init

--- flake.nix
+++ flake.nix
@@ -2,8 +2,10 @@
   description = "Some templates for flakes ill use in projects";
   outputs = { self }: {
     templates = {
-      path = ./generic;
-      description = "A generic DevFlake Template";
+      generic = {
+        path = ./generic;
+        description = "A generic DevFlake Template";
+      };
     };
   };
 }
